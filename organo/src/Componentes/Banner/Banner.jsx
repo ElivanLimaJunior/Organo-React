e89ae6324a265353f04d056f7c29d4a0e6acb17d
ElivanLimaJunior <85586631+ElivanLimaJunior@users.noreply.github.com>
2022-10-21 16:02:50 -0300

    Criando Banner
    Criamos o componente Banner e apagamos alguns arquivos que vem junto com o NPX do Creat que não vamos usar agora.

---
+++ organo/src/Componentes/Banner/Banner.jsx
@@ -0,0 +1,10 @@
+import "./Banner.css"
+function banner () {
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="Banner principal"></img>
+        </header>
+    )
+}
+
+export default banner
