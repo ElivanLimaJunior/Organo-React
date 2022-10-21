3d1db608c38d0d07a822c0733370ca69b91ea9e1
ElivanLimaJunior <85586631+ElivanLimaJunior@users.noreply.github.com>
2022-10-21 16:24:19 -0300

    Modo de import Index
    É possível também fazer o import sem repetir diversas vezes o nome do arquivo.
    Se nomearmos o arquivo "JSX" como 'Index.jsx", então não será necessário no import do componente colocarmos o nome index.js novamente.

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,5 +1,5 @@
-import Banner from './Componentes/Banner/Banner.jsx';
-import CampoTexto from './Componentes/Formulario/CampoTexto.jsx';
+import Banner from './Componentes/Banner/';
+import CampoTexto from './Componentes/Formulario/';
 
 function App() {
   return (
