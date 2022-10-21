ac8991848dc77db7774df9c2f7638610e16648aa
ElivanLimaJunior <85586631+ElivanLimaJunior@users.noreply.github.com>
2022-10-21 16:22:02 -0300

    Criando componente de formulário

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,8 +1,12 @@
-import Banner from './Componentes/Banner/Banner';
+import Banner from './Componentes/Banner/Banner.jsx';
+import CampoTexto from './Componentes/Formulario/CampoTexto.jsx';
 
 function App() {
   return (
+    <div className='app'>
       <Banner/>
+      <CampoTexto/>
+    </div>
   )
 }
 
