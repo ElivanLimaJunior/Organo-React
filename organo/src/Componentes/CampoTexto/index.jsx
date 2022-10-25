208f55a5c36474ee1dc247758310dfa934a0899c
ElivanLimaJunior <85586631+ElivanLimaJunior@users.noreply.github.com>
2022-10-25 15:31:18 -0300

    Manipulando input do usuário
    Usamos o evento onChange e atribuimos a função aoDigitar.

--- organo/src/Componentes/CampoTexto/index.jsx
+++ organo/src/Componentes/CampoTexto/index.jsx
@@ -4,12 +4,19 @@ const CampoTexto = (props) => {
 
     const placeholderModificada = `${props.placeholder} . . .`
 
+    let valor = 'Elivan'
+
+    const aoDigitar = (evento) => {
+        valor = evento.target.value
+        console.log(valor)
+    }
+
     return (
         <div className="campo-texto">
             <label htmlFor="">
                 {props.label}
             </label>
-            <input required={props.obrigatorio} placeholder={placeholderModificada} type="text" />
+            <input value={valor} onChange={aoDigitar} required={props.obrigatorio} placeholder={placeholderModificada} type="text" />
         </div>
     )
 }
