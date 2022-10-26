a0cc0d0ffb447dc04984d5a79889a4f90e17f2c6
ElivanLimaJunior <85586631+ElivanLimaJunior@users.noreply.github.com>
2022-10-26 15:02:25 -0300

    Criando Card Colaborador
    Criamos o estilizamos o Card dos Colaboradores. O Css por ser mt complexo, pegamos pronto o que havia sido disponibilizado para todos, visto que o curso é de React.Js e não de CSS. :)

---
+++ organo/src/Componentes/Colaborador/index.jsx
@@ -0,0 +1,15 @@
+import './Colaborador.css'
+
+export const Colaborador = () => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src="https://github.com/ElivanLimaJunior.png" alt="Elivan Junior" />
+            </div>
+            <div className='rodape'>
+                <h4>Elivan Junior</h4>
+                <h5>Dev Front</h5>
+            </div>
+        </div>
+    )
+}
--- organo/src/Componentes/Time/index.jsx
+++ organo/src/Componentes/Time/index.jsx
@@ -1,3 +1,4 @@
+import { Colaborador } from '../Colaborador'
 import './Time.css'
 
 export const Time = (props) => {
@@ -9,6 +10,7 @@ export const Time = (props) => {
 
         <section className='time' style={css}>
             <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+            <Colaborador />
         </section>
     )
 }
